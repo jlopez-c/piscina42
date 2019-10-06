ab79a22dda75a246db163fd98458661644272c8e
Jesus antonio Lopez centeno <jlopez-c@c2r4s2.42madrid.com>
2019-10-06 10:37:38 +0200

    mis ejercicios

---
+++ c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlopez-c <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/04 14:23:21 by jlopez-c          #+#    #+#             */
+/*   Updated: 2019/10/04 14:35:27 by jlopez-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		ft_putchar(c);
+		c++;
+	}
+}
+
+int		main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}
