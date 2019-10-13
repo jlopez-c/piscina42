979d4953f74581401de4767fc7cbc09b01efaa64
jlopez-c <55802228+jlopez-c@users.noreply.github.com>
2019-10-13 13:03:25 +0200

    New ex
    New ex

--- c00/ex05/ft_print_comb.c
+++ c00/ex05/ft_print_comb.c
@@ -1,25 +1,40 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_print_comb.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: jlopez-c <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/06 18:19:54 by jlopez-c          #+#    #+#             */
-/*   Updated: 2019/10/09 14:50:31 by jlopez-c         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void    ft_putchar(char c)
 {
-	write(1, &c, 1);
+  write(1, &c, 1);
 }
 
-
-
-void	ft_print_comb(void)
+void    ft_print_comb(void)
 {
+    int number[3];
 
+    number[0] = '0';
+    while (number[0] <= '9')
+    {
+        number[1] = number[0] + 1;
+        while (number[1] <= '9')
+        {
+            number[2] = number[1] + 1;
+            while (number[2] <= '9')
+            {
+                ft_putchar(number[0]);
+                ft_putchar(number[1]);
+                ft_putchar(number[2]);
+                if (!(number[0] == '7' && number[1] == '8' && number[2] == '9'))
+                {
+                    ft_putchar(',');
+                    ft_putchar(' ');
+                }
+                number[2]++;
+            }
+            number[1]++;
+        }
+        number[0]++;
+    }
+}
+
+int main() {
+  ft_print_comb();
+  return 0;
 }
